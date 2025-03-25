311185cc717a8d8dd239f3243cf0d37c4a4895ec
nilenos <182097464+nilenos@users.noreply.github.com>
2025-03-25 10:40:07 +0200

    Update matopeli.py

--- matopeli.py
+++ matopeli.py
@@ -62,7 +62,7 @@ def peli():
 
         while peli_loppu:
             ruutu.fill(valkoinen)
-            viesti("Hävisit! Enter = Uudestaan, Esc = Lopeta", punainen)
+            viesti("Pelaa uudestaan. Hävisit! Enter = Uudestaan, Esc = Lopeta", punainen)
             pisteet(maton_pituus - 1)
             pygame.display.update()
 
